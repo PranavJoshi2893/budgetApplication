444c2db75fa98e13ace901e907d5ba0207f20783
PranavJoshi2893 <pranavjoshi2893@gmail.com>
2022-09-09 18:20:24 +0530

    Edit index.html and index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let budgetAmmount=0;
+let BudgetBtn=document.getElementById('budgetBtn');
+let ReloadBtn=document.getElementById('reloadBtn');
+BudgetBtn.style.visibility='visible';
+ReloadBtn.style.visibility='hidden';
 
 function budgetBtnClicked(){
+    
     let BudgetAmt=document.getElementById('budgetAmt');
-    console.log(BudgetAmt.value);
+    BudgetBtn.style.visibility='hidden';
+    ReloadBtn.style.visibility='visible';
 }
